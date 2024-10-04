c864eb69c6ddf6e92f0042677e4e924d9c5f9ef7
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-04 22:11:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+  const FirstVariable = prompt("Enter the number");
+    const SecondVariable= prompt("Enter the secount number");
+    
+    const result=(FirstVariable*2)+ +SecondVariable;
+
+    const FinalResult= alert("The final result is: " + result);
